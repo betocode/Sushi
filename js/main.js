315166dd40fdc9f854623245e60e8c25f4fed300
Roberto <robertojr_@outlook.com.br>
2019-05-15 05:22:33 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  $(".navbar-toggler").click(function() {
+    $(".navbar-toggler").toggleClass("change");
+  });
+
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if (position >= 718) {
+      $("#main-nav").addClass("navbar-background");
+      $("#main-nav").addClass("fixed-top");
+    } else {
+      $("#main-nav").removeClass("navbar-background");
+      $("#main-nav").removeClass("fixed-top");
+    }
+  });
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if (position >= 800) {
+      $("#back-to-top").addClass("scrollTop");
+    } else {
+      $("#back-to-top").removeClass("scrollTop");
+    }
+  });
+  // Add smooth scrolling to all links
+  $("a").on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
